bf1cd3d3fbe80ea4a4a4a48702e72cbc52a01aee
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-06-25 14:59:38 +0200

    creating Gradle project in IntelliJ Java Tribes

---
+++ src/main/java/com/greenfox/javatribes/javatribes/JavaTribesApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.javatribes.javatribes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTribesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTribesApplication.class, args);
+    }
+
+}
