68ea42b39b2801a6ee0b8d32cf81f3e0e9cb82c8
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-07-03 14:18:54 +0200

    little changes in CustomUserDetailsServiceImpl.class

---
+++ src/main/java/com/greenfox/javatribes/javatribes/service/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.greenfox.javatribes.javatribes.service;
+
+import com.greenfox.javatribes.javatribes.model.CustomUserDetails;
+import com.greenfox.javatribes.javatribes.model.User;
+import com.greenfox.javatribes.javatribes.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByUsername(username);
+
+        optionalUser
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return optionalUser
+                .map(CustomUserDetails::new).get();
+    }
+}
