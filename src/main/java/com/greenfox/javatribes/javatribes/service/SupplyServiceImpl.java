f418628bb558ac627c63edd2dd899cac0b14d03d
McGeorgan <juro.seky@gmail.com>
2019-07-17 12:30:43 +0200

    SupplyTimerService recreated

--- src/main/java/com/greenfox/javatribes/javatribes/service/SupplyServiceImpl.java
+++ src/main/java/com/greenfox/javatribes/javatribes/service/SupplyServiceImpl.java
@@ -6,7 +6,6 @@ import com.greenfox.javatribes.javatribes.repositories.SupplyRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -49,13 +48,6 @@ public class SupplyServiceImpl implements SupplyService {
 
         findById(id).setAmount(findById(id).getAmount() + findById(id).getGeneration());
         supplyRepository.save(findById(id));
-
-    }
-
-    @Scheduled(fixedRate = 1000)
-    public void earnSuppliesEverySecond() {
-
-        earnById(1);
 
     }
 
--- src/main/java/com/greenfox/javatribes/javatribes/service/SupplyTimerService.java
+++ src/main/java/com/greenfox/javatribes/javatribes/service/SupplyTimerService.java
@@ -1,6 +1,4 @@
 package com.greenfox.javatribes.javatribes.service;
-
-import com.greenfox.javatribes.javatribes.model.Supply;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,23 +6,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableScheduling
-public class TimeService {
+public class SupplyTimerService {
 
     @Autowired
     UserService userService;
     @Autowired
     SupplyService supplyService;
 
-    Supply supply = new Supply("gold",5,5);
-
     @Scheduled(fixedRate = 1000)
     public void scheduleFixedRateTask() {
 
-        int x = 0;
+
         supplyService.earnById(1);
-        System.out.println(x);
-        x = x + 1;
-        supply.setAmount(supply.getAmount()+supply.getGeneration());
+
 
     }
 
