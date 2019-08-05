df6b75e8d18f817be0b1fa3199e8c8d4068157f8
Stanislav Koumar <stanislav.koumar@gmail.com>
2019-08-05 11:59:03 +0200

    just one comment in CommandLineRunner

--- src/main/java/com/greenfox/javatribes/javatribes/JavaTribesApplication.java
+++ src/main/java/com/greenfox/javatribes/javatribes/JavaTribesApplication.java
@@ -1,6 +1,9 @@
 package com.greenfox.javatribes.javatribes;
 
 import com.greenfox.javatribes.javatribes.dto.RegisterObject;
+import com.greenfox.javatribes.javatribes.model.Building;
+import com.greenfox.javatribes.javatribes.model.Role;
+import com.greenfox.javatribes.javatribes.model.Troop;
 import com.greenfox.javatribes.javatribes.model.User;
 import com.greenfox.javatribes.javatribes.service.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -29,9 +32,11 @@ public class JavaTribesApplication implements CommandLineRunner {
 
         User admin = userService.register(adminObject);
         User user = userService.register(userObject);
+        userService.saveUser(user);
+        userService.saveUser(admin);
 
+//        MAKE CONFLICT TO TESTS - APPLICATION CONTEXT FAILED, COMMAND LINE RUNNER ERROR - NEED TO SOLVE
 //        admin.addRole(Role.ROLE_ADMIN);
-
 //        admin.getKingdom().addBuilding(new Building("townhall"));
 //        admin.getKingdom().addTroop(new Troop());
 
